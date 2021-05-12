3d824f2b14277768ebc0ea8dfe41b4aefe39d30f
HungBee <trieutanhung93@gmail.com>
2021-05-12 18:48:16 +0700

    Update Room UI and Feature UI

--- server/config/adafruit.config.js
+++
@@ -1,9 +0,0 @@
-const args = require("minimist")(process.argv.slice(2))
-
-module.exports = {
-    HOST: "io.adafruit.com",
-    PORT: 8883,
-    IO_USERNAME: "nhkhang",
-    IO_PASSWORD: args['IO_PASSWORD'],
-    FEED_LED: "nhkhang/feeds/bk-iot-led"
-};
--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const dataRoutes = require("./data.routes");
-
-router.post('/light', dataRoutes.light);
-
-module.exports = router;
