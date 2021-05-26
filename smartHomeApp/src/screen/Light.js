f9c2d8a692b96214f02e7fcc9eb6b9687fa65345
HungBee <trieutanhung93@gmail.com>
2021-05-26 13:24:59 +0700

    Change Light Ui Genral

--- smartHomeApp/src/screen/Light.js
+++ smartHomeApp/src/screen/Light.js
@@ -4,7 +4,7 @@ import { View, Text, TouchableOpacity, FlatList, Switch} from "react-native";
 import styles from '../style/screen'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import LightData from '../data/LightData';
-
+import RoomsData from '../data/RoomsData'
 
 function filter(data, id) {
     if(id === "0")
@@ -94,26 +94,99 @@ class LightList extends Component {
         )
     }
 }
+var groups;
 
-// function groupByKey(array, key) {
-//     return array
-//         .reduce((hash, obj) => {
-//             if(obj[key] === undefined) return hash; 
-//             return Object.assign(hash, { [obj[key]]:( hash[obj[key]] || [] ).concat(obj)})
-//         }, {})
-// }
+class LightGeneral extends Component{
+    constructor(){
+        super();
+        this.state = {
+            data: groups
+        }
+    }
+    //const [listLight, setListLight] = React.useState(filter(LightData,itemChoose));
 
-// function grouping (data){
-//     return groupByKey(data, 'room')
-// }
+    const setLightState = (value, index) => {
+        const tempData = _.cloneDeep(listLight);
+        tempData[index].state = value ? "1" : "0";
+        setListLight(tempData);
+    }
+
+    const LightItem = ({item,index}) => (
+        <View style={styles.lightCard}>
+            <View style={styles.lightItem}>
+                <View style={styles.headerLightItem}>
+                    <Text style={styles.nameLight}>{item.name}</Text>
+                    <Switch
+                        value={item.state == "1" ? true : false}
+                        style={styles.toggleLight}
+                        onValueChange={(value) => setLightState(value,index)}
+                    />
+                </View>
+                <View style={styles.bodyLightItem}>
+                    <MaterialCommunityIcons style={item.state == "1"?styles.lightOn:styles.lightOff} name={item.state == "1"?'lightbulb-on':'lightbulb-off'} size={50} color={"#000000"} />
+                </View>
+            </View>
+        </View>
+    )
+    lightGeneralItem = ({name, list}) =>(
+        <View style={styles.rowLightMode}>
+            <Text style={styles.titleRow}>name</Text>
+        </View>
+        <View style={styles.lightSystemMode}>
+            <ScrollView>
+                <FlatList
+                    data ={list}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({item,index}) => (
+                        <LightItem item={item} index={index}/>
+                    )}>
+                </FlatList>
+            </ScrollView>
+        </View>
+    )
+    render(){
+        var data = this.state.data;
+
+        Object.keys(data).forEach(key =>{
+            let value = data[key];
+            console.log(`${key}: ${value}`);
+        })
+        return(
+            <Text>Light General</Text>
+        )
+    }
+}
+
+function groupByKey(array, key) {
+    return array
+        .reduce((hash, obj) => {
+            if(obj[key] === undefined) return hash; 
+            return Object.assign(hash, { [obj[key]]:( hash[obj[key]] || [] ).concat(obj)})
+        }, {})
+}
+
+function grouping(data){
+    return groupByKey(data, 'room')
+}
 
 function LightScreen({route}) {
     const {name, id} = route.params;
-    data = filter(LightData, id);
-    countLeft = Math.ceil(data.length/2);
-    return (
-        <LightList/>
-    );
+    if(id==0){
+        groups = grouping(LightData);
+        console.log(groups);
+        return(
+            <LightGeneral/>
+        )
+        
+    }
+    else{
+        data = filter(LightData, id);
+        countLeft = Math.ceil(data.length/2);
+        return (
+            <LightList/>
+        );
+    }
 }
 
 export default LightScreen;
