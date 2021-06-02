2053897719017bd391210b9d74fc1b332a06007b
ldkhang4869 <ldkhang4869@gmail.com>
2021-06-02 20:39:34 +0700

    Fix error

--- smartHomeApp/src/mqtt/messHandler.js
+++ smartHomeApp/src/mqtt/messHandler.js
@@ -11,8 +11,6 @@ class MessHandler {
         storeData("relay", LightData);
         storeData("door", DoorData);
         storeData("room", RoomsData);
-
-        console.log(JSON.stringify(data));
     }
     handleRelay(data) {
         updateData("relay", data);
