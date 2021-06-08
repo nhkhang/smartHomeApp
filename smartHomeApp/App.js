88aa43e8731e28a733f3790793bb1ab42b44d613
nvkhang-cse <nv.khang.khmt@gmail.com>
2021-06-08 20:12:41 +0700

    update module login

--- smartHomeApp/App.js
+++ smartHomeApp/App.js
@@ -6,6 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { AuthContext } from './src/api/context';
 import SignIn from './src/screen/SignIn';
 import SignUp from './src/screen/SignUp'
+import {login, register} from '../server/controller/AuthController'
 import {mqtt} from './src/mqtt/MQTT';
 import init from 'react_native_mqtt';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -73,7 +74,7 @@ function App ({navigation}) {
         //Process with 'data' to signin
         console.log(data);
 
-        if(data.username == "abc" && data.password == "123"){
+        if(login(data) == 'Login Successful!'){
           dispatch({type: "SIGN_IN", token: "dummy-auth-token"});
         }
       },
